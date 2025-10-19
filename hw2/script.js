af8a8ded5fa3e6317da2f76ff952da0f818d4114
SHAWON-THE-KING <shawonislam443@gmail.com>
2025-10-19 22:05:23 +0600

    js togglr

---
+++ hw2/script.js
@@ -0,0 +1,20 @@
+// let img1 = document.querySelector(".img-1");
+// let img2 = document.querySelector(".img-2");
+// let btn = document.querySelector(".btn");
+// let body = document.querySelector("body");
+
+// function action() {
+//     if (img2.style.display == "none") {
+//         img2.style.display = "block";
+//     } else {
+//         img2.style.display = "none";
+//     }
+// }
+
+// btn.addEventListener("click",action);
+
+
+function myFun() {
+    let icon2 = document.querySelector(".icon-2");
+    icon2.classList.toggle("js-style")
+}
